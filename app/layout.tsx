4bea62af4b575a19680c2faf1f902a327c3e55b7
ebukaamadiobi <ebukaamadiobi@gmail.com>
2025-12-19 14:08:46 +0000

    added homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,21 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import AppleGaramond from "next/font/local";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
+const appleGaramond = AppleGaramond({
+  src: [
+    { path: "./fonts/AppleGaramond/AppleGaramond.ttf", weight: "400", style: "normal" },
+    { path: "./fonts/AppleGaramond/AppleGaramond-Italic.ttf", weight: "400", style: "italic" },
+  ],
+  variable: "--font-apple-garamond",
+  display: "swap",
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -25,8 +35,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${appleGaramond.variable} antialiased`}
       >
+          
         {children}
       </body>
     </html>
